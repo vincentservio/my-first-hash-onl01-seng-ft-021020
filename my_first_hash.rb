9f86d2201f63ef1466227ebe842a266699ba7674
Vincent Servio <vincentservio@gmail.com>
2020-02-13 23:37:50 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -32,7 +32,7 @@ def adding
                   "oil paintings" => 3
                  }
                  
-                 sh
+                 shipping_manifest
   # add 2 muskets to the shipping_manifest hash below
 
 
